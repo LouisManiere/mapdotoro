9f2313f7d33c8cc4eb17c10ef4a471db49c969f3
Manière Louis <lmanie01@dsi.ens-lyon.fr>
2023-09-14 15:40:11 +0200

    change export to dev isig database and annd region and bassin data

--- data-raw/export_to_pg.R
+++ data-raw/export_to_pg.R
@@ -7,25 +7,32 @@ library(readr)
 
 
 con <- DBI::dbConnect(RPostgres::Postgres(),
-                      host   = "localhost",
-                      dbname = "dbmapdo",
-                      user      = Sys.getenv("DBMAPDO_LOCALHOST_USER"),
-                      password  = Sys.getenv("DBMAPDO_LOCALHOST_PASS"),
-                      port     = 5432)
+                      host = Sys.getenv("DBMAPDO_DEV_HOST"),
+                      port = Sys.getenv("DBMAPDO_DEV_PORT"),
+                      dbname = Sys.getenv("DBMAPDO_DEV_NAME"),
+                      user      = Sys.getenv("DBMAPDO_DEV_USER"),
+                      password  = Sys.getenv("DBMAPDO_DEV_PASS"))
 
 
 
-network_strahler <- st_read("data-raw/network_strahler.gpkg")
-continuity <- read.csv2("data-raw/continuity.csv")
-metrics <- read.csv2("data-raw/metrics.csv")
-landcover <- read.csv2("data-raw/landcover.csv")
+# network_strahler <- st_read("data-raw/network_strahler.gpkg")
+# continuity <- read.csv2("data-raw/continuity.csv")
+# metrics <- read.csv2("data-raw/metrics.csv")
+# landcover <- read.csv2("data-raw/landcover.csv")
 network_metrics <- st_read("data-raw/network_landcover_continuity_metrics.gpkg")
 network_axis <- st_read("data-raw/network_axis.gpkg")
+bassin_hydro <- st_read(dsn = "data-raw/dbmapdo.gpkg", layer = "bassin_hydrographique")
+region_hydro <- st_read(dsn = "data-raw/dbmapdo.gpkg", layer = "region_hydrographique")
+# secteur_hydro <- st_read(dsn = "data-raw/dbmapdo.gpkg", layer = "secteur_hydrographique")
 
-st_write(network_strahler, con, "network", driver = "PostgreSQL")
-dbWriteTable(con, "continuity", continuity, overwrite = TRUE, row.names = FALSE)
-dbWriteTable(con, "metrics", metrics, overwrite = TRUE, row.names = FALSE)
-dbWriteTable(con, "landcover", landcover, overwrite = TRUE, row.names = FALSE)
+
+st_write(bassin_hydro, con, "bassin_hydrographique", driver = "PostgreSQL")
+st_write(region_hydro, con, "region_hydrographique", driver = "PostgreSQL")
+
+# st_write(network_strahler, con, "network", driver = "PostgreSQL")
+# dbWriteTable(con, "continuity", continuity, overwrite = TRUE, row.names = FALSE)
+# dbWriteTable(con, "metrics", metrics, overwrite = TRUE, row.names = FALSE)
+# dbWriteTable(con, "landcover", landcover, overwrite = TRUE, row.names = FALSE)
 st_write(network_metrics, con, "network_metrics", driver = "PostgreSQL")
 st_write(network_axis, con, "network_axis", driver = "PostgreSQL")
 
