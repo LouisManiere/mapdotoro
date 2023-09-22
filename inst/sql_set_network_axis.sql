88d8997df02967def041a873159362bed5c91c5e
Manière Louis <lmanie01@dsi.ens-lyon.fr>
2023-09-22 15:00:21 +0200

    update sql network axis and roe

--- inst/sql_set_network_axis.sql
+++ inst/sql_set_network_axis.sql
@@ -6,12 +6,12 @@ DECLARE row record;
 BEGIN
   FOR row IN SELECT table_schema,table_name,column_name
              FROM information_schema.columns
-             WHERE table_schema = 'public' AND 
+             WHERE table_schema = 'public' AND
              table_name   = 'network_axis'
   LOOP
   IF row.column_name != lower(row.column_name) THEN
     EXECUTE format('ALTER TABLE %I.%I RENAME COLUMN %I TO %I',
-      row.table_schema,row.table_name,row.column_name,lower(row.column_name));  
+      row.table_schema,row.table_name,row.column_name,lower(row.column_name));
 	  END IF;
   END LOOP;
 END $$;
@@ -24,8 +24,22 @@ ALTER TABLE network_axis
 ADD CONSTRAINT network_axis_unq_axis
 UNIQUE (axis);
 
-CREATE INDEX idx_fid_network_axis 
+CREATE INDEX idx_fid_network_axis
 ON network_axis USING btree(fid);
 
-CREATE INDEX idx_geom_network_axis 
+CREATE INDEX idx_geom_network_axis
 ON network_axis USING GIST (geom);
+
+-- add gid_region
+ALTER TABLE network_axis
+ADD COLUMN gid_region integer;
+
+-- update gid_region with spatial join in buffer to fix some intersection
+UPDATE network_axis AS nm
+SET gid_region = rh.gid
+FROM region_hydrographique AS rh
+WHERE ST_Within(ST_TRANSFORM(nm.geom, 2154), ST_Buffer(ST_TRANSFORM(rh.geom, 2154), 1000));
+
+-- création d'un index sur gid_region
+CREATE INDEX idx_gid_region_network_axis
+ON network_axis USING btree(gid_region);
--- inst/sql_set_roe.sql
+++ inst/sql_set_roe.sql
@@ -6,12 +6,12 @@ DECLARE row record;
 BEGIN
   FOR row IN SELECT table_schema,table_name,column_name
              FROM information_schema.columns
-             WHERE table_schema = 'public' AND 
+             WHERE table_schema = 'public' AND
              table_name   = 'roe'
   LOOP
   IF row.column_name != lower(row.column_name) THEN
     EXECUTE format('ALTER TABLE %I.%I RENAME COLUMN %I TO %I',
-      row.table_schema,row.table_name,row.column_name,lower(row.column_name));  
+      row.table_schema,row.table_name,row.column_name,lower(row.column_name));
 	  END IF;
   END LOOP;
 END $$;
@@ -30,3 +30,17 @@ ON roe USING btree(cdobstecou);
 
 CREATE INDEX idx_geom_roe
 ON roe USING GIST (geom);
+
+-- add gid_region
+ALTER TABLE roe
+ADD COLUMN gid_region integer;
+
+-- update gid_region with spatial join
+UPDATE roe AS nm
+SET gid_region = rh.gid
+FROM region_hydrographique AS rh
+WHERE ST_Within(nm.geom, rh.geom);
+
+-- création d'un index sur gid_region
+CREATE INDEX idx_gid_region_roe
+ON roe USING btree(gid_region);
